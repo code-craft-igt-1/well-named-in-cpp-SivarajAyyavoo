b5c640b016b8ed53683872b2a8e4bb71bf41b7a8
SivarajAyyavoo <41547753+SivarajAyyavoo@users.noreply.github.com>
2024-08-01 09:24:51 +0530

    Refactor the code
    Make sure LOC of each file is less than 50

---
+++ telcocolorcoder.h
@@ -0,0 +1,26 @@
+#ifndef TELCOCOLORCODER_H
+#define TELCOCOLORCODER_H
+
+#include <string>
+
+namespace TelCoColorCoder
+{
+	enum MajorColor { WHITE, RED, BLACK, YELLOW, VIOLET };
+	enum MinorColor { BLUE, ORANGE, GREEN, BROWN, SLATE };
+
+	class ColorPair {
+		private:
+			MajorColor majorColor;
+			MinorColor minorColor;
+		public:
+			ColorPair(MajorColor major, MinorColor minor);
+			MajorColor getMajor();
+			MinorColor getMinor();
+			std::string ToString();
+	};
+
+	ColorPair GetColorFromPairNumber(int pairNumber);
+	int GetPairNumberFromColor(MajorColor major, MinorColor minor);
+}
+
+#endif
